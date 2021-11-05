694ff36ed2fa4bdee329c5b91b0507b2ce5e5b8b
Matthew Ames <mga321@gmail.com>
2021-11-05 15:37:59 -0400

    Re-format program notes for readability

--- scraper_pt_1.py
+++ scraper_pt_1.py
@@ -13,7 +13,7 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 
 
-# Collect link from primary article (top left of webpage, the left portion of html section <div class="tw2" section)
+# Step 1:  Collect link from primary article (top left of webpage, the left portion of html section <div class="tw2" section)
 secure_links = []
 
 primary_article = soup.find("span", class_="tw2_l_t")
@@ -24,7 +24,7 @@ secure_links.append(secure_link)
 
 
 
-# Collect links from secondary articles (top right of webpage. the right portion of html section <div class="tw2" section)
+# Step 2:  Collect links from secondary articles (top right of webpage. the right portion of html section <div class="tw2" section)
 secondary_articles = soup.find_all("div", class_="tBox2")
 
 for secondary_article in secondary_articles:
@@ -35,9 +35,7 @@ for secondary_article in secondary_articles:
 
 
 
-# Collect links from list of articles (center of webpage below the primary and secondary articles section, the list of <div class="mb10 tw3_01_2" sections)
-
-
+# Step 3:  Collect links from list of articles (center of webpage below the primary and secondary articles section, the list of <div class="mb10 tw3_01_2" sections)
 articles_list = soup.find_all("span", class_="tw3_01_2_t")
 
 for listed_article in articles_list:
