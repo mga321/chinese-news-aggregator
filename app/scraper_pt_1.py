9aa51b9d0d522e5225c5954e410c199cf410aa38
Matthew Ames <mga321@gmail.com>
2021-11-15 13:45:34 -0500

    Re-write link collection code as user defined functions and re-run program to confirm success

--- app/scraper_pt_1.py
+++ app/scraper_pt_1.py
@@ -5,56 +5,70 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# Article Links Func 1:  Collect link from primary article (top left of webpage, the left portion of html section <div class="tw2" section)
+def collect_primary_article_link(soup_object, list_of_secure_links):
+    """This function takes a soup object and a list as inputs, parses thru the soup object to find
+    primary article link (main article at the top left of the page), and appends the link to the list
+    of secure links."""
+
+    # Collect from webpage primary article
+    primary_article = soup_object.find("span", class_="tw2_l_t")
+    primary_article_link = primary_article.a.get("href")
+    secure_link = "https:" + primary_article_link
+
+    list_of_secure_links.append(secure_link)
+
+
+# Article Links Func 2:  Collect links from secondary articles (top right of webpage. the right portion of html section <div class="tw2" section)
+def collect_secondary_article_links(soup_object, list_of_secure_links):
+    """This function takes a soup object and a list as inputs, parses thru the soup object to find
+    secondary article links (main articles at the top right of the page), and appends the links to the list
+    of secure links."""
+    
+    secondary_articles = soup_object.find_all("div", class_="tBox2")
+
+    for secondary_article in secondary_articles:
+        secondary_article_link = secondary_article.a.get("href")
+        secure_link = "https:" + secondary_article_link
+
+        list_of_secure_links.append(secure_link)
+
+
+# Article Links Func 3:  Collect links from list of articles (center of webpage below the primary and secondary articles section, the list of <div class="mb10 tw3_01_2" sections)
+def collect_listed_article_links(soup_object, list_of_secure_links):
+    """This function takes a soup object and a list as inputs, parses thru the soup object to find
+    listed articles links (list of articles in the center of the page), and appends the links to the list
+    of secure links."""
+
+    articles_list = soup_object.find_all("span", class_="tw3_01_2_t")
+
+    for listed_article in articles_list:
+        listed_article_link = listed_article.a.get("href")
+        secure_link = "https:" + listed_article_link
+
+        list_of_secure_links.append(secure_link)
+
+
+
+
 # Change to chinese-news-aggregator directory
 app_directory = Path.home() / "OneDrive - Jobin Machine, Inc/2021/chinese-news-aggregator/app"
 os.chdir(app_directory)
 
 
-
-# Collect html code from china-us section page on China Daily website
+# Collect html code from china-us section page on China Daily website and create soup object
 url = "https://www.chinadaily.com.cn/world/china-us#"
 page = requests.get(url)
-# print(page.status_code)
-# print(page.text)
 
-# Create a soup object containing all of the web page's html content
 soup = BeautifulSoup(page.content, "html.parser")
 
 
-
-# Article Links Part 1:  Collect link from primary article (top left of webpage, the left portion of html section <div class="tw2" section)
+# Collect article links
 secure_links = []
+funcs = [collect_primary_article_link, collect_secondary_article_links, collect_listed_article_links]
 
-primary_article = soup.find("span", class_="tw2_l_t")
-primary_article_link = primary_article.a.get("href")
-secure_link = "https:" + primary_article_link
-
-secure_links.append(secure_link)
-
-
-
-# Article Links Part 2:  Collect links from secondary articles (top right of webpage. the right portion of html section <div class="tw2" section)
-secondary_articles = soup.find_all("div", class_="tBox2")
-
-for secondary_article in secondary_articles:
-    secondary_article_link = secondary_article.a.get("href")
-    secure_link = "https:" + secondary_article_link
-
-    secure_links.append(secure_link)
-
-
-
-# Article Links Part 3:  Collect links from list of articles (center of webpage below the primary and secondary articles section, the list of <div class="mb10 tw3_01_2" sections)
-articles_list = soup.find_all("span", class_="tw3_01_2_t")
-
-for listed_article in articles_list:
-    listed_article_link = listed_article.a.get("href")
-    secure_link = "https:" + listed_article_link
-
-    secure_links.append(secure_link)
-
-# print(secure_links)
-
+for func in funcs:
+    func(soup, secure_links)
 
 
 # Create json file containing all potential new links for the current day
