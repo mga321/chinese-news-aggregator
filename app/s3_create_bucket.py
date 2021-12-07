40af18ba996fb0cd8279b90a12bfdb3f08e92e79
Matt Ames <mga321@gmail.com>
2021-12-07 10:26:51 -0500

    Rename variables

--- app/s3_create_bucket.py
+++ app/s3_create_bucket.py
@@ -1,13 +1,13 @@
 import boto3
 
-AWS_REGION = "us-east-2"
+aws_region = "us-east-2"
 
-s3_resource = boto3.resource("s3", region_name=AWS_REGION)
+s3_resource = boto3.resource("s3", region_name=aws_region)
 
 
 # Create new bucket
 bucket_name = "daily-data-collection"
-location = {"LocationConstraint": AWS_REGION}
+location = {"LocationConstraint": aws_region}
 
 bucket = s3_resource.create_bucket(
     Bucket=bucket_name,
