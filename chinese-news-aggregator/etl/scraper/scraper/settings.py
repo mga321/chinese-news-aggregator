6dabcc01602dbadf8e9f8ccee2645ad6f7c0fa5a
Matt <mga321@gmail.com>
2022-02-11 16:47:26 -0500

    Replace web scraper version 1.0 directory structure with version 2.0
    The directory structure used for the initial basic web scraper using Beautiful Soup and Requests has been replaced with the structure used for the Scrapy framework web scraper

--- chinese-news-aggregator/etl/scraper/scraper/settings.py
+++ chinese-news-aggregator/etl/scraper/scraper/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for chinese_news_scraper project
+# Scrapy settings for scraper project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'chinese_news_scraper'
+BOT_NAME = 'scraper'
 
-SPIDER_MODULES = ['chinese_news_scraper.spiders']
-NEWSPIDER_MODULE = 'chinese_news_scraper.spiders'
+SPIDER_MODULES = ['scraper.spiders']
+NEWSPIDER_MODULE = 'scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'chinese_news_scraper (+http://www.yourdomain.com)'
+#USER_AGENT = 'scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'chinese_news_scraper.middlewares.ChineseNewsScraperSpiderMiddleware': 543,
+#    'scraper.middlewares.ScraperSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'chinese_news_scraper.middlewares.ChineseNewsScraperDownloaderMiddleware': 543,
+#    'scraper.middlewares.ScraperDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'chinese_news_scraper.pipelines.ChineseNewsScraperPipeline': 300,
+#    'scraper.pipelines.ScraperPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
