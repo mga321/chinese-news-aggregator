a2a3f25e7ea4001876447e38bea3ade285574c52
Matt <mga321@gmail.com>
2022-02-26 15:56:52 -0500

    Update comments and method documentation for china_daily_spider

--- chinese-news-aggregator/etl/scraper/scraper/spiders/china_daily_spider.py
+++ chinese-news-aggregator/etl/scraper/scraper/spiders/china_daily_spider.py
@@ -2,6 +2,7 @@ import scrapy
 import datetime
 
 
+# Get the current date in string format
 todays_date_string = datetime.datetime.today().strftime('%Y-%m-%d')
 
 
@@ -12,21 +13,23 @@ class ChinaDailySpider(scrapy.Spider):
 
 
     def parse(self, response):
-        """Define the primary, secondary, and tertiary sections as well as the functionality here."""
+        """This method parses through the list of articles on the main page of the "China-US" section where all
+        articles in this category are listed. It finds all articles with the current date and returns a response object
+        by following the url of each article. It then passes the response object for each article url into the parse_article_content method."""
 
         articles = response.css('div.mb10.tw3_01_2')  # Create a subset of data containing all articles from the response
 
-        for article in articles:  # For each article in articles response subset
-            if article.css('b::text').get()[:10] == todays_date_string:  # If the date of the article is today's date
+        for article in articles:  # For each article in articles response object
+            if article.css('b::text').get()[:10] == todays_date_string:  # Check if the date of the article is today's date
 
                 yield response.follow(article.css('a').attrib['href'], callback=self.parse_article_content)
     
 
     def parse_article_content(self, response):
-        """Documentation here."""
+        """This function parses the response object yielded by the original parse method and yields the required data in a dictionary."""
         
-        current_url = response.request.url
-        article_content = response.css('div.lft_art')
+        current_url = response.request.url  # Store the response object's url attribute in a variable
+        article_content = response.css('div.lft_art')  # Create a subset of the data contianing all of the relevant article data
 
         yield {
             'date': todays_date_string,
