41e26a6475b746d9863534f90750362dd378c116
Matt <mga321@gmail.com>
2022-03-09 18:43:13 -0500

    Change file paths used for S3 upload module

--- chinese-news-aggregator/etl/s3/s3_upload_files.py
+++ chinese-news-aggregator/etl/s3/s3_upload_files.py
@@ -6,4 +6,4 @@ import constants
 # Change to chinese-news-aggregator directory
 os.chdir(constants.DATA_DIRECTORY)
 
-helpers.upload_file("article-data-from-new-links.json", "daily-data-collection")
+helpers.upload_file("test.json", "daily-data-collection")
